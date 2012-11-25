8c77563f887e1ad04ff06beaa7dcc300a55bb111
Daniel Garcia Moreno <danigm@wadobo.com>
2012-11-25 23:30:06 +0100

    A lot of css and basic blog stuffs
    * Two columns
    * feeds
    * tags page
    * archive page
    * pages dev example page

---
+++ content/pages/dev.md
@@ -0,0 +1,48 @@
+Date: 2012-11-24 12:10
+Title: Devs
+Slug: devs
+
+#### Desarrollos Propios
+
+Cada vez que hago un pequeño script o algún programa acaba por
+desaparecer en mi sistema de ficheros y me gustaría compartirlo con
+más gente, así que a partir de ahora todo lo que haga acabará colgado
+en mi repositorio personal.
+
+#### Ramas (git)
+
+Forja de sugus:
+
+SUGUS forge
+
+Otros desarrollos en los que participo:
+
+gnome
+github
+gitorious
+Launchpad https://code.launchpad.net/~danigm
+GoogleCode http://code.google.com/u/danigm/
+
+
+* #### `cpp` - C++
+
+        :::cpp
+        #include <iostream.h>
+        using namespace std;
+
+        int main() {
+          cout << "¡Hola, mundo!" << endl;
+          return 0;
+        }
+
+* #### `python` - Python
+
+        :::python
+        import os
+
+        for file in os.path.listdir():
+            print file
+
+        cass PRUF:
+            def __init__(self):
+                self.prurf = "hola"
--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ SITEURL = 'http://danigm.net'
 
 TIMEZONE = 'Europe/Paris'
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = 'es'
 
 # Blogroll
 LINKS =  (
@@ -23,9 +23,45 @@ SOCIAL = (
     ('facebook', 'http://facebook.com/danigmx'),
 )
 
-DISQUS_SITENAME = "danigm.net"
+DISQUS_SITENAME = "danigm"
 TWITTER_USERNAME = "danigm"
 
-DEFAULT_PAGINATION = 10
+#A list of the extensions that the markdown processor will use.
+MD_EXTENSIONS = ['codehilite', 'extra']
+
+# The static paths you want to have accessible on the output path “static”. By
+# default, pelican will copy the ‘images’ folder to the output folder.
+# static paths will be copied under the same name
+STATIC_PATHS = ["pictures", ]
+
+# relative url to output the atom feed.
+FEED_ATOM = "atom.xml"
+
+# relative url to output the rss feed.
+FEED_RSS = 'rss.xml'
+
+CATEGORY_FEED_RSS = 'feeds/cat/%s.rss.xml'
+CATEGORY_FEED_ATOM = 'feeds/cat/%s.atom.xml'
+
+# relative url to output the tags atom feed. It should be defined using a “%s”
+# matchin the tag name
+TAG_FEED_ATOM = 'feeds/tags/%s.atom.xml'
+
+# relative url to output the tag RSS feed
+TAG_FEED_RSS = "feeds/tags/%s.rss.xml"
+
+
+DEFAULT_PAGINATION = 3
+WITH_PAGINATION = True
+
+# Count of different font sizes in the tag cloud.
+TAG_CLOUD_STEPS = 4
+
+# Maximum tags count in the cloud.
+TAG_CLOUD_MAX_ITEMS = 200
+
+DISPLAY_PAGES_ON_MENU = True
+
 THEME = "danigm-theme"
+#THEME = "dev-random"
 
